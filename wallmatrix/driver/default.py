fa718510584f98e253c4abaf489442b736d2d765
Brooke Chalmers <breq@breq.dev>
2021-09-05 14:30:40 -0400

    Add a fake driver for MacOS

--- wallmatrix/driver/default.py
+++ wallmatrix/driver/default.py
@@ -1,8 +1,12 @@
 import os
+import platform
 
 if os.environ["DRIVER"] == "real":
     from wallmatrix.driver.real import RealMatrixDriver as Driver
 elif os.environ["DRIVER"] == "fake":
-    from wallmatrix.driver.fake import FakeMatrixDriver as Driver
+    if platform.system() == "Darwin":
+        from wallmatrix.driver.fake_macos import FakeMatrixDriver as Driver
+    else:
+        from wallmatrix.driver.fake import FakeMatrixDriver as Driver
 
 __all__ = ["Driver"]
